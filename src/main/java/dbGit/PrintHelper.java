5790c9287da7a1270181a300f4b18ceaad2e0d60
Mohamed Anees <anees.m@practo.com>
2019-08-04 15:15:58 +0530

    Init commit for v2

--- src/main/java/dbGit/PrintHelper.java
+++ src/main/java/dbGit/PrintHelper.java
@@ -34,37 +34,33 @@ public class PrintHelper {
   private static void printViewBranchesHelpCommands() {
     System.out.println("\n View all available branches to a database\n");
     System.out
-        .println("\nVIEW:\n dbgit view <address:port> <db_name> <db_user_name> <db_password>");
+        .println("\nVIEW:\n dbgit view");
     System.out.println("Example\n");
-    System.out.println("dbgit view localhost:5432 mytestdb postgres mypass123");
-    System.out.println("Please enter **null** for empty fields");
+    System.out.println("dbgit view");
   }
 
   private static void printCheckoutBranchHelpCommands() {
     System.out.println("\n Checkout to available branch to a database\n");
     System.out.println(
-        "\nCHECKOUT:\n dbgit checkout <address:port> <db_name> <db_user_name> <db_password> <branch_name>");
+        "\nCHECKOUT:\n dbgit checkout <branch_name>");
     System.out.println("Example\n");
-    System.out.println("dbgit checkout localhost:5432 mytestdb postgres mypass123 mybranch");
-    System.out.println("Please enter **null** for empty fields");
+    System.out.println("dbgit checkout mybranch");
   }
 
   private static void printDropBranchHelpCommands() {
     System.out.println("\n Drop available branch to a database \n");
     System.out.println(
-        "\nDROP:\n dbgit drop <address:port> <db_name> <db_user_name> <db_password> <branch_name>");
+        "\nDROP:\n dbgit drop <branch_name>");
     System.out.println("Example\n");
-    System.out.println("dbgit drop localhost:5432 mytestdb postgres mypass123 mybranch");
-    System.out.println("Please enter **null** for empty fields");
+    System.out.println("dbgit drop mybranch");
   }
 
   private static void printInitBranchHelpCommands() {
     System.out.println("\n Create a branch to a database \n");
     System.out.println(
-        "\nINIT:\n dbgit init <address:port> <db_name> <db_user_name> <db_password> <branch_name>");
+        "\nINIT:\n dbgit init <branch_name>");
     System.out.println("Example\n");
-    System.out.println("dbgit init localhost:5432 mytestdb postgres mypass123 mybranch");
-    System.out.println("Please enter **null** for empty fields");
+    System.out.println("dbgit init mybranch");
   }
 
 
